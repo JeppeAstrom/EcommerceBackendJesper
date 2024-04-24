7d7d7d60a137df03f900e6d88ad45f2652d71b4a
Jeppe <you@example.com>
2024-04-24 23:09:57 +0200

    payment

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,10 +1,13 @@
 ﻿using EcommerceBackend.Models.Schemas;
 using examensarbete_backend.Contexts;
 using Manero_Backend.Helpers.JWT;
+using Manero_Backend.Models.Dtos.Address;
+using Manero_Backend.Models.Dtos.PaymentDetail;
 using Manero_Backend.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceBackend.Controllers
 {
@@ -46,6 +49,17 @@ namespace EcommerceBackend.Controllers
             {
                 return StatusCode(500, e);
             }
+        }
+        [HttpGet("paymentdetail")]
+        public async Task<ActionResult<PaymentDetailDto>> GetPaymentDetail()
+        {
+            var userId = JwtToken.GetIdFromClaim(HttpContext);
+            PaymentDetailDto payment = await _context.PaymentDetails.Where(a => a.AppUserId == userId).FirstOrDefaultAsync();
+            if (payment == null)
+            {
+                return NotFound();
+            }
+            return payment;
         }
     }
 }
