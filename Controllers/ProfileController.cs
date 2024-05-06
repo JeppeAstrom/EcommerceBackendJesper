1a9b4602bd9b4b3daa463e917f0466c52f4f9c16
Jeppe <you@example.com>
2024-05-06 15:03:30 +0200

    fix

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,53 @@
+﻿using examensarbete_backend.Contexts;
+using Manero_Backend.Helpers.JWT;
+using Manero_Backend.Models.Dtos.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceBackend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProfileController : ControllerBase
+{
+    private readonly DatabaseContext _context;
+
+    public ProfileController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<UserProfileDto>> GetProfile()
+    {
+        var userId = JwtToken.GetIdFromClaim(HttpContext);
+
+        if(userId == null)
+        {
+            return NotFound();
+        }
+
+        var userProfile = await _context.Users.Where(u => u.Id == userId).FirstOrDefaultAsync();
+
+
+        if (userProfile == null)
+        {
+            return BadRequest("");
+        }
+
+        var profileDto = new UserProfileDto
+        {
+            FirstName = userProfile.FirstName,
+            LastName = userProfile.LastName,
+            Email = userProfile.Email,
+            ImageUrl = userProfile.ImageUrl,
+            PhoneNumber = userProfile.PhoneNumber,
+            Location = userProfile.Location,
+        };
+
+        return profileDto;
+    }
+}
